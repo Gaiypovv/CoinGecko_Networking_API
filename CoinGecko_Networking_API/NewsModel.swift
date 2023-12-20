1b3042978e6c8e59583bec4c6a0c12000156eb01
Eldar <114824844+Gaiypovv@users.noreply.github.com>
2023-12-20 10:55:10 +0600

    added the NewsModel and NewsView

---
+++ CoinGecko_Networking_API/NewsModel.swift
@@ -0,0 +1,27 @@
+//
+//  NewsModel.swift
+//  CoinGecko_Networking_API
+//
+//  Created by Eldar Gaiypov on 20/12/23.
+//
+
+import Foundation
+
+struct NewsModel: Codable {
+    let status: String?
+    let totalResults: Int?
+    let articles: [Article]?
+    
+    enum CodingKeys: String, CodingKey {
+        case status = "status"
+        case totalResults = "totalResults"
+        case articles = "articles"
+    }
+}
+
+struct Arcticle: Codable, Hashable {
+    let title: String?
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+}
--- CoinGecko_Networking_API/NewsView.swift
+++ CoinGecko_Networking_API/NewsView.swift
@@ -6,13 +6,74 @@
 //
 
 import SwiftUI
+import SafariServices
+
 
 struct NewsView: View {
+    @State var news: NewsModel?
+
+    let test: Article = Article.init(title: "loading...", description: "loading...", url: "loading...", urlToImage: "loading...")
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                ForEach((news?.articles) ?? [test], id: \.self) { item in
+                    HStack {
+                        VStack {
+                            AsyncImage(url: URL(string: "\(item.urlToImage ?? "")")) { image in
+                                image.resizable()
+
+                            } placeholder: {
+                                ProgressView()
+                            }
+                                .cornerRadius(20)
+                                .frame(width: 400, height: 300)
+                            Spacer()
+                            HStack {
+                                Text("\(item.title ?? "")")
+                                    .font(.headline)
+                                .frame(alignment: .leading)
+                                Link(destination: URL(string: "\(item.url ?? "")")!) {
+                                    Text("Visit Site")
+                                }
+                            }
+                            Text("\(item.description ?? "")")
+                                .font(.subheadline)
+                            Divider()
+                        }
+
+                            .onTapGesture {
+                            print("Hello World")
+                        }
+                    }
+                }
+                    .onAppear() {
+                    NewsAPICall().getAPI { (data) in
+                        self.news = data
+                    }
+                }
+            }
+                .navigationBarTitle("Crypto News")
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarBackButtonHidden(true)
+                .toolbar {
+                Button(action: {
+
+                    print("Reload")
+
+                    NewsAPICall().getAPI { (data) in
+                        self.news = data
+                    }
+                }) {
+                    Text("Reload")
+                }
+            }
+        }
     }
 }
 
-#Preview {
-    NewsView()
+struct NewsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewsView()
+    }
 }
+
