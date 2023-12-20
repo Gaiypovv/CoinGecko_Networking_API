708fa6a826861163688248578027b51f91a903be
Eldar <114824844+Gaiypovv@users.noreply.github.com>
2023-12-20 10:58:09 +0600

    added the ExchagesView

---
+++ CoinGecko_Networking_API/ExchangesView.swift
@@ -0,0 +1,70 @@
+//
+//  ExchangesView.swift
+//  CoinGecko_Networking_API
+//
+//  Created by Eldar Gaiypov on 20/12/23.
+//
+
+import SwiftUI
+import SafariServices
+
+@State var data: [ExchangeModel]?
+
+let tmp = ExchangeModel.init(id: "", name: "", yearEstablished: 0, country: "", url: "", image: "", trustScore: 0, trustScoreRank: 0, tradeVolume24HBtc: 0, tradeVolume24HBtcNormalized: 0.0)
+var body: some View {
+    NavigationView {
+        List {
+            ForEach(data ?? [tmp], id: \.self) { item in
+                HStack {
+                    Text("\(item.trustScoreRank ?? 0)")
+                    AsyncImage(url: URL(string: "\(item.image ?? "")")) { image in
+                        image.resizable()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                        .frame(width: 20, height: 20)
+                    VStack(alignment: .leading) {
+                        Text("\(item.name ?? "")")
+                            .font(.headline)
+                        HStack {
+                            Text("\(item.country ?? "Country Unkown")")
+                                .font(.footnote)
+                            let yourIntValue = item.yearEstablished ?? 0
+                            Text(String(yourIntValue))
+                                .font(.footnote)
+                        }
+                    }
+                    Spacer()
+                    VStack {
+                        Text("₿\(item.tradeVolume24HBtcNormalized ?? 0, specifier: "%.0f")")
+                    }
+                }
+            }
+        }
+            .navigationBarTitle("Top 100 Exchanges")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+            Button(action: {
+
+                print("Reload")
+
+                ExchangeAPICall().getAPI { (data) in
+                    self.data = data
+                }
+            }) {
+                Text("Reload")
+            }
+        }
+    }
+        .onAppear() {
+        ExchangeAPICall().getAPI { (data) in
+            self.data = data
+        }
+    }
+}
+}
+
+#Preview {
+    ExchangesView()
+}
