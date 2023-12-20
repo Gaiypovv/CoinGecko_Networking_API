4e651d784bdf8177c7b4b46772bf23368e4ab4fb
Eldar <114824844+Gaiypovv@users.noreply.github.com>
2023-12-20 11:15:28 +0600

    added the Controller file and implement the APICall class

---
+++ CoinGecko_Networking_API/Contoller.swift
@@ -0,0 +1,31 @@
+//
+//  Contoller.swift
+//  CoinGecko_Networking_API
+//
+//  Created by Eldar Gaiypov on 20/12/23.
+//
+
+import Foundation
+
+class APICall {
+    func getAPI(complition: @escaping ([Main]) -> ()) {
+        guard let url = URL(string:
+                                "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false" ) else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            
+            do {
+                let returnedData = try JSONDecoder().decode([Main].self, from: data!)
+                
+                DispatchQueue.main.async {
+                    complition(returnedData)
+                }
+            } catch {
+                print(String(describing: error))
+            }
+        }
+        .resume()
+    }
+}
