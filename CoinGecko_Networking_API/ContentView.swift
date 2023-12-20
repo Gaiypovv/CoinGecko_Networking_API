abaf9a13cb57496dcbefe0391d76b9ffe87ce146
Eldar <114824844+Gaiypovv@users.noreply.github.com>
2023-12-20 10:38:35 +0600

    added the CoinDetailData view

--- CoinGecko_Networking_API/ContentView.swift
+++ CoinGecko_Networking_API/ContentView.swift
@@ -102,3 +102,114 @@ struct MainView: View {
     }
 }
 
+struct CoinDetailedData: View {
+    let coin: Main
+
+    var body: some View {
+        VStack {
+            List {
+                Section(header: Text("\(coin.name ?? "")")) {
+                    HStack {
+                        Text("Market Cap")
+                        Spacer()
+                        Text("$\(coin.marketCap ?? 0)")
+                    }
+                    HStack {
+                        Text("Fully Diluted Valuation")
+                        Spacer()
+                        Text("$\(coin.fullyDilutedValuation ?? 0)")
+                    }
+                }
+                Section(header: Text("24-hour Price Data")) {
+                    HStack {
+                        Text("24-Hour High")
+                        Spacer()
+                        Text("$\(coin.high24H ?? 0.0, specifier: "%.2f")")
+                    }
+                    HStack {
+                        Text("24-Hour Low")
+                        Spacer()
+                        Text("$\(coin.low24H ?? 0.0, specifier: "%.2f")")
+                    }
+                    if coin.priceChange24H ?? 0 < 0.0 {
+                        HStack {
+                            Text("Price Change 24h")
+                            Spacer()
+                            Image(systemName: "arrow.down")
+                                .foregroundColor(.red)
+                            Text("$\(coin.priceChange24H ?? 0, specifier: "%.2f")")
+                                .foregroundColor(.red)
+                        }
+
+                    } else if coin.priceChange24H ?? 0 > 0.0 {
+                        HStack {
+                            Text("Price Change 24h")
+                            Spacer()
+                            Image(systemName: "arrow.up")
+                                .foregroundColor(.green)
+                            Text("$\(coin.priceChange24H ?? 0, specifier: "%.2f")")
+                                .foregroundColor(.green)
+                        }
+                    }
+
+                    if coin.priceChangePercentage24H ?? 0 < 0.0 {
+                        HStack {
+                            Text("Price Change % 24h")
+                            Spacer()
+                            Image(systemName: "arrow.down")
+                                .foregroundColor(.red)
+                            Text("\(coin.priceChangePercentage24H ?? 0, specifier: "%.2f")%")
+                                .foregroundColor(.red)
+                        }
+                    } else if coin.priceChangePercentage24H ?? 0 > 0.0 {
+                        HStack {
+                            Text("Price Change % 24h")
+                            Spacer()
+                            Image(systemName: "arrow.up")
+                                .foregroundColor(.green)
+                            Text("\(coin.priceChangePercentage24H ?? 0, specifier: "%.2f")%")
+                                .foregroundColor(.green)
+                        }
+                    }
+                }
+                Section(header: Text("Historical Data")) {
+                    HStack {
+                        Text("All Time Price")
+                        Spacer()
+                        Text("$\(coin.ath ?? 0.0, specifier: "%.2f")")
+                    }
+                    if coin.priceChangePercentage24H ?? 0 < 0.0 {
+                        HStack {
+                            Text("All Time high % Change:")
+                            Spacer()
+                            Text("\(coin.athChangePercentage ?? 0.0, specifier: "%.2f")%")
+                                .foregroundColor(.red)
+                        }
+                    } else if coin.priceChangePercentage24H ?? 0 < 0.0 {
+                        HStack {
+                            Text("All Time high % Change:")
+                            Spacer()
+                            Text("\(coin.athChangePercentage ?? 0.0, specifier: "%.2f")%")
+                                .foregroundColor(.red)
+                        }
+                    }
+                }
+
+                Section(header: Text("Supply Data")) {
+                    HStack {
+                        Text("Circulating Supply")
+                        Spacer()
+                        Text("\(coin.circulatingSupply ?? 0, specifier: "%.0f")")
+                    }
+                    HStack {
+                        Text("Total Supply")
+                        Spacer()
+                        Text("\(coin.totalSupply ?? 0, specifier: "%.0f")")
+                    }
+                }
+                
+            }
+        }
+    }
+}
+
