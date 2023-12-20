1147d01c3a45e88943d2bc0889d23415b77e196f
Eldar <114824844+Gaiypovv@users.noreply.github.com>
2023-12-20 10:49:04 +0600

    added the NewsView

---
+++ CoinGecko_Networking_API/NewsView.swift
@@ -0,0 +1,18 @@
+//
+//  NewsView.swift
+//  CoinGecko_Networking_API
+//
+//  Created by Eldar Gaiypov on 20/12/23.
+//
+
+import SwiftUI
+
+struct NewsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewsView()
+}
